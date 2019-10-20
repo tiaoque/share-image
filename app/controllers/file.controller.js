d74c0ff8d29fa6c14060cfaa48eed272ad9c2181
nopains <nopains@126.com>
2019-10-20 17:13:59 +0800

    first commit

---
+++ app/controllers/file.controller.js
@@ -0,0 +1,42 @@
+var stream = require('stream');
+
+const db = require('../config/db.config.js');
+const File = db.files;
+
+exports.uploadFile = (req, res) => {
+	File.create({
+		type: req.file.mimetype,
+		name: req.file.originalname,
+		data: req.file.buffer
+	}).then(() => {
+		res.json({msg:'File uploaded successfully! -> filename = ' + req.file.originalname});
+	}).catch(err => {
+		console.log(err);
+		res.json({msg: 'Error', detail: err});
+	});
+}
+
+exports.listAllFiles = (req, res) => {
+	File.findAll({attributes: ['id', 'name']}).then(files => {
+	  res.json(files);
+	}).catch(err => {
+		console.log(err);
+		res.json({msg: 'Error', detail: err});
+	});
+}
+
+exports.downloadFile = (req, res) => {
+	File.findById(req.params.id).then(file => {
+		var fileContents = Buffer.from(file.data, "base64");
+		var readStream = new stream.PassThrough();
+		readStream.end(fileContents);
+		
+		res.set('Content-disposition', 'attachment; filename=' + file.name);
+		res.set('Content-Type', file.type);
+
+		readStream.pipe(res);
+	}).catch(err => {
+		console.log(err);
+		res.json({msg: 'Error', detail: err});
+	});
+}
